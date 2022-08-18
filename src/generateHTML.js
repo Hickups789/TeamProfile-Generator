c12c4e633f17c6a88f928a44428dbe03bee06fbc
Hickups789 <jackysantiago358@yahoo.com>
2022-08-17 21:24:43 -0400

    finishing touch css

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -14,7 +14,7 @@ const generateManager = function (manager) {
         </div>
     </div>
 </div>
-`
+`;
 };
 
 const generateIntern = function (intern) {
@@ -33,11 +33,11 @@ const generateIntern = function (intern) {
         </div>
     </div>
 </div>
-    `
+    `;
 };
 
 const generateEngineer = function (engineer) {
-    return `
+  return `
     <div class = "col-4 mt-4">
     <div class="card h-100">
         <div class="card-header">
@@ -52,29 +52,26 @@ const generateEngineer = function (engineer) {
         </div>
     </div>
 </div>
-    `
+    `;
 };
 
-const generateCards = (team)=> {
-    console.log("in the generateCards")
-    let cards = ""
-    for (let i = 0; i < team.length; i++) {
-        if(team[i].getRole() === "Manager"){
-            cards += generateManager(team[i])
-        }else if(team[i].getRole() === "Intern"){
-            cards += generateIntern(team[i])
-        }else if(team[i].getRole() === "Engineer"){
-            cards += generateEngineer(team[i])
-        }
-
-        
-       
-      }
-    return cards
-}
+const generateCards = (team) => {
+  console.log("in the generateCards");
+  let cards = "";
+  for (let i = 0; i < team.length; i++) {
+    if (team[i].getRole() === "Manager") {
+      cards += generateManager(team[i]);
+    } else if (team[i].getRole() === "Intern") {
+      cards += generateIntern(team[i]);
+    } else if (team[i].getRole() === "Engineer") {
+      cards += generateEngineer(team[i]);
+    }
+  }
+  return cards;
+};
 
 const generateTeam = (teamArray) => {
-    return`
+  return `
     <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -82,6 +79,7 @@ const generateTeam = (teamArray) => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Team Profile Generator</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="../dist/style.css" />
   </head>
   <body>
@@ -100,19 +98,11 @@ const generateTeam = (teamArray) => {
       </div>
     </main>
   </body>
-  <script
-    src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-    crossorigin="anonymous"
-  ></script>
-  <script
-    src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-    crossorigin="anonymous"
-  ></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </html>
-    `
+    `;
 };
 
-
-module.exports = generateTeam
+module.exports = generateTeam;
